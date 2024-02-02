8f8cd113a37f8e83cbff234170786a828b8c25b7
MarcosEduard <109650197+MarcosEduard@users.noreply.github.com>
2024-02-02 17:54:44 -0500

    Add files via upload

---
+++ PROBANDO/FormularioSinParametro/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace FormularioSinParametro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnListarClientes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(121, 28);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(537, 205);
+            this.dgvCustomers.TabIndex = 0;
+            this.dgvCustomers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCustomers_CellContentClick);
+            // 
+            // btnListarClientes
+            // 
+            this.btnListarClientes.Location = new System.Drawing.Point(321, 298);
+            this.btnListarClientes.Name = "btnListarClientes";
+            this.btnListarClientes.Size = new System.Drawing.Size(134, 78);
+            this.btnListarClientes.TabIndex = 1;
+            this.btnListarClientes.Text = "Lista de Clientes";
+            this.btnListarClientes.UseVisualStyleBackColor = true;
+            this.btnListarClientes.Click += new System.EventHandler(this.btnListarClientes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListarClientes);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnListarClientes;
+    }
+}
+
---
+++ PROBANDO/FormularioSinParametro/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FormularioSinParametro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Código que se ejecuta cuando el formulario se carga
+            // Puedes agregar lógica adicional si es necesario
+        }
+
+        private void dgvCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Evento que se ejecuta cuando se hace clic en una celda del DataGridView
+            // Puedes agregar lógica adicional si es necesario
+        }
+
+        private void btnListarClientes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declaración de objetos SqlDataAdapter y DataTable
+                SqlDataAdapter da;
+                DataTable dt;
+
+                // Crear un SqlDataAdapter y asociarlo con el procedimiento almacenado "ConsultaOrden"
+                // Conectar.Conexion() es una función que devuelve una conexión a la base de datos
+                da = new SqlDataAdapter("ConsultaOrden", Conectar.Conexion());
+
+                // Crear un DataTable para almacenar los resultados de la consulta
+                dt = new DataTable();
+
+                // Llenar el DataTable con los resultados de la consulta
+                da.Fill(dt);
+
+                // Asignar el DataTable como origen de datos para el DataGridView
+                dgvCustomers.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                // Capturar y mostrar cualquier excepción que pueda ocurrir durante la ejecución
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
