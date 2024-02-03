a70561ec929e35d602d017c6d7791b7bdc8037de
MarcosEduard <109650197+MarcosEduard@users.noreply.github.com>
2024-02-02 21:02:35 -0500

    Add files via upload

---
+++ probandoParametro/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace probandoParametro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnListarClientes = new System.Windows.Forms.Button();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(101, 36);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(578, 212);
+            this.dgvCustomers.TabIndex = 0;
+            this.dgvCustomers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCustomers_CellContentClick);
+            // 
+            // btnListarClientes
+            // 
+            this.btnListarClientes.Location = new System.Drawing.Point(190, 306);
+            this.btnListarClientes.Name = "btnListarClientes";
+            this.btnListarClientes.Size = new System.Drawing.Size(123, 58);
+            this.btnListarClientes.TabIndex = 1;
+            this.btnListarClientes.Text = "Lista de Clientes";
+            this.btnListarClientes.UseVisualStyleBackColor = true;
+            this.btnListarClientes.Click += new System.EventHandler(this.btnListarClientes_Click);
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.Location = new System.Drawing.Point(411, 323);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(100, 20);
+            this.txtParametro.TabIndex = 2;
+            this.txtParametro.TextChanged += new System.EventHandler(this.txtParametro_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtParametro);
+            this.Controls.Add(this.btnListarClientes);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnListarClientes;
+        private System.Windows.Forms.TextBox txtParametro;
+    }
+}
+
---
+++ probandoParametro/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace probandoParametro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnListarClientes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declaración de objetos SqlDataAdapter y DataTable
+                SqlDataAdapter da;
+                DataTable dt;
+
+                // Obtener el valor del parámetro desde un TextBox o cualquier otro control
+                string customerID = txtParametro.Text;
+
+                // Crear una instancia de SqlDataAdapter con el nombre del procedimiento almacenado y la conexión
+                da = new SqlDataAdapter("ConsultaOrdenParametro", Conectar.Conexion());
+
+                // Especificar que el comando es un Stored Procedure
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                // Agregar el parámetro al comando
+                da.SelectCommand.Parameters.AddWithValue("@CustomerID", customerID);
+
+                // Crear un DataTable para almacenar los resultados
+                dt = new DataTable();
+
+                // Llenar el DataTable con los resultados de la consulta
+                da.Fill(dt);    
+
+                // Asignar el DataTable como origen de datos para el DataGridView
+                dgvCustomers.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                // Capturar y mostrar cualquier excepción que pueda ocurrir durante la ejecución
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtParametro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
